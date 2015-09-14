e944136b4b27899267b26bc137fd518c81d6af44
ThinkBigZach <zachary.wilson@thinkibiganalytics.com>
2015-09-14 12:00:43 -0600

    DDD

--- src/main/java/FileMapper.java
+++ src/main/java/FileMapper.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -14,14 +15,17 @@ public class FileMapper {
 
     static String testing ="hdfs://quickstart.cloudera:8020/financialDataFeed/data/8764/athena/finished/2011-07-01-2011-07-31/";
     static String practiceID;
-    static String outPath = "/user/rscott22/mapping/";
+//    static String outPath = "/user/rscott22/mapping/";
+    static String outPath = "/user/athena/financialdatafeed/extracted/finished";
     static String entity;
     static String fileName;
+    static FileSystem fs;
     static HashMap<String, ArrayList<String>> mapping;
     public static Path getControlPath(String pathToControl) throws IOException {
-        FileSystem fs = FileSystem.newInstance(new Configuration());
+        fs = FileSystem.newInstance(new Configuration());
         FileStatus[] fileStatuses = fs.listStatus(new Path(testing));
         mapping = new HashMap<String, ArrayList<String>>();
+        readFilesFromPath(new Path(testing));
         for(FileStatus status : fileStatuses){
             BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(status.getPath())));
             String line = "";
@@ -57,9 +61,7 @@ public class FileMapper {
                 }
                 FSDataOutputStream out = fs.append(new Path(outPath + s + ".txt"));
                 for(String link : mapping.get(s)){
-
                     out.writeUTF(link + "\n");
-
                 }
                 out.close();
             }
@@ -69,14 +71,30 @@ public class FileMapper {
 
 
     public static void main(String[] args) throws IOException {
-        for(String s: args){
-            System.out.println(s);
-        }
         testing = args[0];
+        entity = args[1];
+
+
         getControlPath(testing);
     }
 
 
+
+
+    public static void readFilesFromPath(Path pathToFiles) throws IOException {
+        FileStatus[] fileStatuses = fs.listStatus(pathToFiles);
+
+        for(FileStatus status : fileStatuses){
+            if(status.isDirectory()){
+                readFilesFromPath(status.getPath());
+            }
+            else{
+                System.out.println(status.getPath().toString());
+
+
+            }
+        }
+    }
 
 
 
