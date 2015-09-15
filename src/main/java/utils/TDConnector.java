44532a222d260decf8ce8a510ec5618cab60a65a
Ramirez <cr186034@muscr186034-524.td.teradata.com>
2015-09-15 10:35:08 -0600

    Teradata connector (TeraDriver should be in classpath

--- src/main/java/utils/TDConnector.java
+++ src/main/java/utils/TDConnector.java
@@ -1,5 +1,30 @@
 package utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class TDConnector {
 
+	public void Connect()
+	{
+		Connection conn;
+		try
+		{
+			Class.forName("com.teradata.jdbc.TeraDriver").newInstance();
+			String url = "jdbc:teradata//dev.teradata.chs.net/DATABASE=EDW_ATHENA_STAGE";
+			conn = DriverManager.getConnection(url, "dbc", "dbc"); //Connection w/user and password
+			if (conn.isValid(0))
+			{
+				System.out.println("Connected to Teradata");
+			}
+			else
+			{
+				System.out.println("Connection to Teradata failed");
+			}
+		} catch (Exception e)
+		{
+			System.out.println("Exception caught: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
 }
