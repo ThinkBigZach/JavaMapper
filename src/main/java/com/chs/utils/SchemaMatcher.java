da77486d0f4705017dbc26e72fb5127dfdd73b30
Ramirez <cr186034@muscr186034-524.td.teradata.com>
2015-09-16 10:16:32 -0600

    TODOs completed

--- src/main/java/com/chs/utils/SchemaMatcher.java
+++ src/main/java/com/chs/utils/SchemaMatcher.java
@@ -3,29 +3,38 @@ package com.chs.utils;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import org.apache.log4j.Logger;
 
 /**
  * @author cr186034
  */
 
-//TODO:  replace replace println statments with logging statements.
-//TODO: make delimineters constants.
+//TODO(1): Put file(path) compareURL into "error array"
 public class SchemaMatcher {
+	
+	private static String delimiter = "\\036";
+	private static Logger LOG = Logger.getLogger(SchemaMatcher.class);
+	
     public static boolean matchSchemas(String goldenURL, String compareURL) throws FileNotFoundException {
         boolean tripwire = false;
-        Scanner goldenFile = new Scanner(new File(goldenURL)).useDelimiter("\\036");
-        Scanner compareFile = new Scanner(new File(compareURL)).useDelimiter("\\036");
+        Scanner goldenFile = new Scanner(new File(goldenURL)).useDelimiter(delimiter);
+        Scanner compareFile = new Scanner(new File(compareURL)).useDelimiter(delimiter);
+        
         int golden_size = cleanStringByColumn(goldenFile.next()).length;
         int file2_size = cleanStringByColumn(compareFile.next()).length;
         //My understanding (via Gary) that RIGHT NOW, it should ONLY match when column counts are the SAME
         if (golden_size == file2_size) {
-            System.out.println("Schema matched! SUCCESS");
+//            System.out.println("Schema matched! SUCCESS");
+            LOG.info("==========Schema match success===========");
             tripwire = true;
         } else {
-            System.out.println(String.format("Comparing schema had different number of columns than Golden. \nMoving \n\t%s \nto Error Array. FAILURE",
-                    compareURL));
-            //TODO:Put file(path) compareURL into "error array"
+//            System.out.println(String.format("Comparing schema had different number of columns than Golden. \nMoving \n\t%s \nto Error Array. FAILURE",
+//                    compareURL));
+        	LOG.info("==========Schema match failed============");
+            //TODO:1
         }
+        goldenFile.close();
+        compareFile.close();
         return tripwire;
     }
 
