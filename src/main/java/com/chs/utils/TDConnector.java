89d288435513024993cd64ec11427f2f4e44b55a
Ramirez <cr186034@muscr186034-524.td.teradata.com>
2015-09-16 12:10:25 -0600

    Can execute queries against TDConnector

--- src/main/java/com/chs/utils/TDConnector.java
+++ src/main/java/com/chs/utils/TDConnector.java
@@ -2,42 +2,61 @@ package com.chs.utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TDConnector {
 
-	private String host;
-	private String user;
-	private String password;
-	private String database;
+	private static String host;
+	private static String user;
+	private static String password;
+	private static String database;
+	private static Connection conn;
 	
-	public TDConnector(String _host, String _user, String _password, String _database) {
+	/***
+	 * Must call init() before attempting to query/connect
+	 * @return
+	 */
+	private static Connection getConnection()
+	{
+		if (conn == null)
+		{
+			try
+			{
+				Class.forName("com.teradata.jdbc.TeraDriver").newInstance();
+				String url = "jdbc:teradata//" + host + "/DATABASE=" + database;
+				conn = DriverManager.getConnection(url, user, password); //Connection w/user and password
+				if (!conn.isValid(0))
+				{
+					throw new Exception("Connection to Teradata failed..");
+				}
+			} catch (Exception e)
+			{
+				System.out.println("Exception caught: " + e.getMessage());
+				e.printStackTrace();
+			}
+		}
+		return conn;
+	}
+	
+	/***
+	 * Sets the connection parameters
+	 * @param _host
+	 * @param _user
+	 * @param _password
+	 * @param _database
+	 */
+	public static void init(String _host, String _user, String _password, String _database) {
 		host=_host;
 		user=_user;
 		password=_password;
 		database=_database;
 	}
 	
-	public void Connect()
+	public void executeQuery(String sql) throws SQLException
 	{
-		Connection conn;
-		try
-		{
-			Class.forName("com.teradata.jdbc.TeraDriver").newInstance();
-			String url = "jdbc:teradata//" + host + "/DATABASE=" + database;
-			conn = DriverManager.getConnection(url, user, password); //Connection w/user and password
-			if (conn.isValid(0))
-			{
-				System.out.println("Connected to Teradata");
-			}
-			else
-			{
-				System.out.println("Connection to Teradata failed");
-			}
-		} catch (Exception e)
-		{
-			System.out.println("Exception caught: " + e.getMessage());
-			e.printStackTrace();
-		}
+		Connection con = getConnection();
+		Statement stmt = conn.createStatement();
+		stmt.executeQuery(sql);
 	}
-	
 }
