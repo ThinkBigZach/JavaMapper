f311e8ba33d5d5b16d868a60797bdd1e3238b67e
JeffrieRubner <jeffrie.rubner@thinkbiganalytics.com>
2015-09-22 11:10:16 -0600

    fixed up logic around control files for pathDriver

--- src/main/java/com/chs/drivers/PathDriver.java
+++ src/main/java/com/chs/drivers/PathDriver.java
@@ -14,11 +14,11 @@ import java.util.Scanner;
 
 public class PathDriver implements Driver {
 
-    private final String CR = "\r"; //carriage return
-    private final String LF = "\n"; //line feed
-    private final String UNIT_SEPARATOR = "\037";
-    private final String RECORD_SEPARATOR = "\036";
-	
+	private final String CR = "\r"; //carriage return
+	private final String LF = "\n"; //line feed
+	private final String UNIT_SEPARATOR = "\037";
+	private final String RECORD_SEPARATOR = "\036";
+
 	private String input_path;
 	private String entity;
 	private String inputParamEntity;
@@ -32,16 +32,16 @@ public class PathDriver implements Driver {
 	private String jobID;
 
 	public PathDriver(String[] args) {
-			input_path = args[0];
-			entity = args[1];
-		    inputParamEntity = args[1];
-			out_path = args[2];
-			practiceMap_path = args[3]; 
-			entityMap_path = args[4]; 
-			TD_Host = args[5];
-			TD_User = args[6]; 
-			TD_Password = args[7]; 
-			TD_Database = args[8];
+		input_path = args[0];
+		entity = args[1];
+		inputParamEntity = args[1];
+		out_path = args[2];
+		practiceMap_path = args[3]; 
+		entityMap_path = args[4]; 
+		TD_Host = args[5];
+		TD_User = args[6]; 
+		TD_Password = args[7]; 
+		TD_Database = args[8];
 	}
 
 	public void start() {
@@ -55,24 +55,37 @@ public class PathDriver implements Driver {
 					String filename = stat.getPath().getName();
 					if(!filename.equalsIgnoreCase("CONTROL.TXT")){
 						String current_entity = filename.split(".")[0];
-						FSDataOutputStream out = fs.append(new Path(out_path + "/" + current_entity + "/" + ".txt"));
+						FSDataOutputStream out = fs.append(new Path(out_path + "/" + current_entity + "/" + current_entity + ".txt"));
 						Scanner fileScanner = new Scanner(fs.open(stat.getPath()));
-			            fileScanner.useDelimiter(RECORD_SEPARATOR);
-			            String line = "";
-			            int lineCount = 0;
-			            while(fileScanner.hasNextLine()) {
-			                line = fileScanner.next();
-			                if(lineCount == 0){
-//			                    TODO: Column Validation Method
-			                }
-			                if (lineCount > 3) {
-			                    String fixedLine = replaceCRandLF(line);
-			                    fixedLine = fixedLine + UNIT_SEPARATOR + "0" + UNIT_SEPARATOR + jobID + UNIT_SEPARATOR + filename;
-			                    out.write((fixedLine + "\n").getBytes());
-			                }
-			                lineCount++;
-			            }
-			            out.close();
+						fileScanner.useDelimiter(RECORD_SEPARATOR);
+						String line = "";
+						int lineCount = 0;
+						while(fileScanner.hasNextLine()) {
+							line = fileScanner.next();
+							if(lineCount == 0){
+								//			                    TODO: Column Validation Method
+							}
+							if (lineCount > 3) {
+								String fixedLine = replaceCRandLF(line);
+								fixedLine = fixedLine + UNIT_SEPARATOR + "0" + UNIT_SEPARATOR + jobID + UNIT_SEPARATOR + filename;
+								out.write((fixedLine + "\n").getBytes());
+							}
+							lineCount++;
+						}
+						out.close();
+					}
+					else {
+						FSDataOutputStream out = fs.append(new Path(out_path + "/control/" + ".txt"));
+						Scanner fileScanner = new Scanner(fs.open(stat.getPath()));
+						fileScanner.useDelimiter(RECORD_SEPARATOR);
+						String line = "";
+						while(fileScanner.hasNextLine()) {
+							line = fileScanner.next();
+							String fixedLine = replaceCRandLF(line);
+							fixedLine = fixedLine.replaceAll("~", UNIT_SEPARATOR) + "\n";
+							out.write((fixedLine).getBytes());
+						}
+						out.close();
 					}
 				}
 			}
@@ -80,30 +93,30 @@ public class PathDriver implements Driver {
 			e.printStackTrace();
 		} 
 	}
-	
-    private void getJobID(FileSystem fs, FileStatus[] status) throws IOException, FileNotFoundException {
-    	FileStatus stat = null;
-    	boolean hasControl = false;
-    	for(FileStatus s : status){
-    		if(s.getPath().getName().equalsIgnoreCase("CONTROL.TXT")){
-    			stat = s;
-    			hasControl = true;
-    		}
-    	}
-    	if(!hasControl){
-    		throw new FileNotFoundException("The input directory does not contain a control file");
-    	}
-    	BufferedReader br=new BufferedReader(new InputStreamReader(fs.open(stat.getPath())));
-        String line=br.readLine();
-        String[] splits = line.split("~");
-        jobID = splits[4];
-        br.close();		
+
+	private void getJobID(FileSystem fs, FileStatus[] status) throws IOException, FileNotFoundException {
+		FileStatus stat = null;
+		boolean hasControl = false;
+		for(FileStatus s : status){
+			if(s.getPath().getName().equalsIgnoreCase("CONTROL.TXT")){
+				stat = s;
+				hasControl = true;
+			}
+		}
+		if(!hasControl){
+			throw new FileNotFoundException("The input directory does not contain a control file");
+		}
+		BufferedReader br=new BufferedReader(new InputStreamReader(fs.open(stat.getPath())));
+		String line=br.readLine();
+		String[] splits = line.split("~");
+		jobID = splits[4];
+		br.close();		
 	}
 
 	private  String replaceCRandLF(String line){
-        line = line.replaceAll(CR, "");
-        line = line.replaceAll(LF, "");
-        line = line.replaceAll(RECORD_SEPARATOR, "");
-        return line;
-    }
+		line = line.replaceAll(CR, " ");
+		line = line.replaceAll(LF, "");
+		line = line.replaceAll(RECORD_SEPARATOR, "");
+		return line;
+	}
 }
