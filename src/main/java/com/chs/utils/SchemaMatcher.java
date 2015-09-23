caff208d8ef6d28e1f13ca5fa51c1bf34f34a128
Ramirez <cr186034@muscr186034-524.td.teradata.com>
2015-09-23 10:57:51 -0600

    SchemaMatcher cleanup

--- src/main/java/com/chs/utils/SchemaMatcher.java
+++ src/main/java/com/chs/utils/SchemaMatcher.java
@@ -16,7 +16,6 @@ import org.apache.log4j.Logger;
  * @author cr186034
  */
 
-//TODO(1): Put file(path) compareURL into "error array"
 public class SchemaMatcher {
 	
 	private static String delimiter = "\036";
@@ -24,6 +23,16 @@ public class SchemaMatcher {
 	private static Logger LOG = Logger.getLogger(SchemaMatcher.class);
 	public static Map<String, List<SchemaRecord>> goldenEntitySchemaMap = TDConnector.getSchemas();
 	
+	//Dynamic Schema Match change >> Checks to make sure column and column data type are equal. All must match to pass.
+	
+	/**
+	 * Dynamic Schema Matcher used to match against a Golden Schema for a given entity
+	 * @param entity 
+	 * @param compareURL
+	 * @param fs
+	 * @return
+	 * @throws FileNotFoundException
+	 */
     public static boolean matchSchemas(String entity, String compareURL, FileSystem fs) throws FileNotFoundException {
         boolean tripwire = false;
         Map<String, String> goldenMap = getGoldenSchema(entity.toLowerCase());
@@ -31,10 +40,7 @@ public class SchemaMatcher {
         Scanner goldenFile = null;
         Scanner compareFile = null;
         String line1 = null;
-        //goldenMap = extractMapFromFile(cleanStringByColumn(goldenFile.next()), cleanStringByColumn(goldenFile.next()));
-        //compareMap = extractMapFromFile(cleanStringByColumn(compareFile.next()), cleanStringByColumn(compareFile.next()));
         try {
-	        //goldenFile = new Scanner(new File(goldenURL)).useDelimiter(delimiter);
 	        compareFile = new Scanner(fs.open(new Path(compareURL))).useDelimiter(delimiter);
 	        compareMap = extractMapFromFile(cleanStringByColumn(compareFile.next()), cleanStringByColumn(compareFile.next()));
 		} catch (Exception e) {
@@ -42,22 +48,19 @@ public class SchemaMatcher {
 			System.out.println(String.format("CompareFile: %s \nLine: %s", compareURL, line1));
 			e.printStackTrace();
 		}
-        //Dynamic Schema Match change >> Checks to make sure column and column data type are equal. All must match to pass.
-        if ((goldenMap != null && compareMap != null)) //&& goldenMap.size() == compareMap.size()) 
+        if ((goldenMap != null && compareMap != null))
         {
         	if (schemaMatch(goldenMap, compareMap, goldenMap.size(), entity))
         	{
-        		//LOG.info("==========Schema match success===========");
+        		//Successful match
         		tripwire = true;        		
         	}
         	else
         	{
-        		LOG.info("==========Schema match failed============");        		
-        		//TODO:1        	
+        		LOG.info("==========Schema match failed============");       
         	}
         } else {
         	LOG.info("====SCHEMA COULD NOT BE MATCHED");
-        	//System.out.println(String.format("Golden map: %s<->%s \nCompareMap: %s<->%s", goldenMap, goldenMap.size(), compareMap, compareMap.size()));
         }
         compareFile.close();
         return tripwire;
@@ -72,15 +75,13 @@ public class SchemaMatcher {
     		int count = 0;
     		for (SchemaRecord sr : recordList)
     		{
-    			//tempmap.put(sr.getColumn_name(), sr.getColumn_id());
     			tempmap.put(sr.getColumn_name().toLowerCase(), count);
-    			//System.out.println(String.format("ADDING -> [%s,%s]", count, sr.getColumn_name()));
     			count++;
     		}
     	}
     	else
     	{
-    		System.out.println(String.format("ENTITY %s IS NULL"));
+    		LOG.info(String.format("ENTITY %s IS NULL", entity));
     	}
     	return tempmap;
     }
@@ -101,29 +102,20 @@ public class SchemaMatcher {
     
     private static boolean schemaMatch(Map<String,String> file1Map, Map<String,String> file2map, int mapSize, String entity)
 	{
-		//Dont forget to return ticket on final
 		int ticket = 0;
-//		for (Entry<String,String> kv : file1Map.entrySet())
-//		{
-//			System.out.println("Golden \n\tTable: " + entity + "\n\tColumn name: " + kv.getKey() + "\n\tColumn id: " + kv.getValue());
-//		}
 		for(Entry<String,String> kv : file2map.entrySet())
 		{
 			String mapkey = kv.getKey().toLowerCase().replaceAll(" ", "_");
-			//Column names match. If column Types match, we got it.
-			//System.out.print("Compare " + mapkey + "..");
-			if (file1Map.containsKey(mapkey))//&& kv.getValue().equals(file1Map.get(mapkey)))
+			if (file1Map.containsKey(mapkey))
 			{
+				//Match
 				ticket++;
-				//System.out.println("MATCHED -> " + mapkey);
 			}
 			else
 			{
-				//ticket--;
-				//System.out.println("MISSED -> NO MATCH");
+				//No Match
 			}
 		}
-		//System.out.println(String.format("MATCH COUNT \t -> %s ? %s", ticket, mapSize));
 		return (ticket >= mapSize);
 	}
 
@@ -133,7 +125,7 @@ public class SchemaMatcher {
                 .replaceAll(spacelimiter, ",").split(",");
     }
     
-    private static Map extractMapFromFile(String[] column_Names, String[] column_Types)
+    private static Map<String,String> extractMapFromFile(String[] column_Names, String[] column_Types)
 	{
 		Map<String, String> map = new HashMap<String, String>();
 		for (int i = 0; i < column_Names.length; i++)
