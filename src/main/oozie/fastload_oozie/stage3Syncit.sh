20613d8c40ff951d5ca9a305c18777e9975b45c6
JeffrieRubner <jeffrie.rubner@thinkbiganalytics.com>
2015-11-05 11:07:07 -0700

    changed oozie ip in stage 3 syncit

--- src/main/oozie/fastload_oozie/stage3Syncit.sh
+++ src/main/oozie/fastload_oozie/stage3Syncit.sh
@@ -16,8 +16,8 @@ else
 		COORD_FILE=CURRENT_OOZIE_COORDINATOR_${MODE}_${dataPartID}_${srcSystemID}
 		if [  -f ./${COORD_FILE} ]; then
 			jobID=$(cat ./${COORD_FILE})
-			oozie job -oozie http://10.1.132.20:11000/oozie -suspend ${jobID} 
-			oozie job -oozie http://10.1.132.20:11000/oozie -kill  ${jobID}
+			oozie job -oozie http://10.1.132.24:11000/oozie -suspend ${jobID} 
+			oozie job -oozie http://10.1.132.24:11000/oozie -kill  ${jobID}
 			rm -f ./${COORD_FILE}  ;
 		fi
 	#fi
@@ -39,7 +39,7 @@ else
 		exit -1
 	fi
 
-	rawJobID=$(oozie job -oozie http://10.1.132.20:11000/oozie -config "/hdfs_mount/user/${uID}/stage-3-oozie/job.properties" -DcoordStart=`date -u "+%Y-%m-%dT%H:00Z"` -DuserName=${uID} -DstageThreeOwner=${stage1UserID} -DstageThreeDataPartition=${dataPartID} -DtdServer=${tdServer} -DtdUserID=${tdUserID} -DtdUserIDPassword=${tdUserIDPassword} -Dentity=${entity} -submit)
+	rawJobID=$(oozie job -oozie http://10.1.132.24:11000/oozie -config "/hdfs_mount/user/${uID}/stage-3-oozie/job.properties" -DcoordStart=`date -u "+%Y-%m-%dT%H:00Z"` -DuserName=${uID} -DstageThreeOwner=${stage1UserID} -DstageThreeDataPartition=${dataPartID} -DtdServer=${tdServer} -DtdUserID=${tdUserID} -DtdUserIDPassword=${tdUserIDPassword} -Dentity=${entity} -submit)
 
 	newJobID=$( echo ${rawJobID} | awk '{print $2}')
 	echo $newJobID > ./${COORD_FILE}
