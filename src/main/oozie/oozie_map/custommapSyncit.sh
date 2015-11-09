9149eb65297e610208d2f35a4c374a74cd96a0f7
CZRam <cnuramirez@hotmail.com>
2015-11-09 15:35:28 -0700

    Fixed misnomers in workflow and renamed appropriate files

--- src/main/oozie/oozie_map/custommapSyncit.sh
+++ src/main/oozie/oozie_map/custommapSyncit.sh
@@ -16,8 +16,8 @@ else
 		COORD_FILE=CURRENT_OOZIE_COORDINATOR_${MODE}_${dataPartID}_${srcSystemID}
 		if [  -f ./${COORD_FILE} ]; then
 			jobID=$(cat ./${COORD_FILE})
-			oozie job -oozie http://10.1.132.20:11000/oozie -suspend ${jobID} 
-			oozie job -oozie http://10.1.132.20:11000/oozie -kill  ${jobID}
+			oozie job -oozie http://10.1.132.24:11000/oozie -suspend ${jobID} 
+			oozie job -oozie http://10.1.132.24:11000/oozie -kill  ${jobID}
 			rm -f ./${COORD_FILE}  ;
 		fi
 	#fi
@@ -26,11 +26,13 @@ else
 		custommapUserID="athena"
 		lowermode="prod"
 		tdServer=prod.teradata.chs.net
-		tdUserIDPassword=inf0rmt1prod3t1
+		tdUserID=athena
+		tdUserIDPassword=tdchs123
 	elif [ "$MODE" ==  "DEV" ]; then
     	custommapUserID="athena"
     	lowerMode="dev"
     	tdServer=dev.teradata.chs.net
+		tdUserID=dbc
     	tdUserIDPassword=dbc
 	else
 		echo "returnCode=NOOP"
@@ -45,7 +47,7 @@ else
 #	hadoop fs -put coordinator.xml /user/${uID}/data/${dataPartID}/oozie/coordinator.xml
 #	hadoop fs -put workflow.xml /user/${uID}/data/${dataPartID}/oozie/workflow.xml
 
-	rawJobID=$(oozie job -oozie http://10.1.132.20:11000/oozie -config "/hdfs_mount/user/${uID}/custommap-oozie/job.properties" -DcoordStart=`date -u "+%Y-%m-%dT%H:00Z"` -DuserName=${uID} -Dstage3CustomMapOwner=${custommapUserID} -DcustomMapDataPartition=${dataPartID} -DtdServer=${tdServer} -DtdUserIDPassword=${tdUserIDPassword} -Dentity=${entity} -submit)
+	rawJobID=$(oozie job -oozie http://10.1.132.24:11000/oozie -config "/hdfs_mount/user/${uID}/custommap-oozie/job.properties" -DcoordStart=`date -u "+%Y-%m-%dT%H:00Z"` -DuserName=${uID} -Dstage3CustomMapOwner=${custommapUserID} -DcustomMapDataPartition=${dataPartID} -DtdServer=${tdServer} -DtdUserID=${tdUserID} -DtdUserIDPassword=${tdUserIDPassword} -Dentity=${entity} -submit)
 
 	newJobID=$( echo ${rawJobID} | awk '{print $2}')
 	echo $newJobID > ./${COORD_FILE}
